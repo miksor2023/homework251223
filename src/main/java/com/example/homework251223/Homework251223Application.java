a67117712e5172aa086b5e9ab87b9e4f57244855
miksor2023 <miksor@yandex.ru>
2024-01-06 11:01:23 +0300

    Commit

---
+++ src/main/java/com/example/homework251223/Homework251223Application.java
@@ -0,0 +1,13 @@
+package com.example.homework251223;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework251223Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework251223Application.class, args);
+	}
+
+}
